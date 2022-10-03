6d336206571ea29d0a214717c798f911591da8ce
Runys77 <rudolfslukstins@inbox.lv>
2022-10-03 16:30:21 +0300

    Flight planner DB

--- FlightPlanner/Controllers/AdminApiController.cs
+++ FlightPlanner/Controllers/AdminApiController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FlightPlanner.Controllers
 {
@@ -11,25 +14,38 @@ namespace FlightPlanner.Controllers
     [ApiController, Authorize]
     public class AdminApiController : ControllerBase
     {
+        private readonly FlightPlannerDbContext _context;
+        public AdminApiController(FlightPlannerDbContext context)
+        {
+            _context = context;
+        }
+
         [Route("flights/{id}")]
         [HttpGet]
         public IActionResult GetFlight(int id)
         {
-            var flight = FlightStorage.GetFlight(id);
-            
-            if (flight == null)
-            {
-                return NotFound();
-            }
-            
-            return Ok(flight);
+            var flight = _context.Flights
+                .Include(f => f.To)
+                .Include(f => f.From)
+                .FirstOrDefault(f => f.Id == id);
+            //var flight = FlightStorage.GetFlight(id);
+             
+             if (flight == null)
+             {
+                 return NotFound();
+             }
+             
+             return Ok(flight);
         }
 
         [Route("flights")]
         [HttpPut]
         public IActionResult PutFlight(Flight flight)
         {
-            if (FlightStorage.IsWrongValues(flight) ||
+            _context.Flights.Add(flight);
+            _context.SaveChanges();
+            
+            /*if (FlightStorage.IsWrongValues(flight) ||
                 flight.From.AirportCode.Trim().ToUpper().Equals(flight.To.AirportCode.Trim().ToUpper()) ||
                 DateTime.Parse(flight.DepartureTime) >= DateTime.Parse(flight.ArrivalTime))
             {
@@ -41,7 +57,7 @@ namespace FlightPlanner.Controllers
                return Conflict();
             }
 
-            flight = FlightStorage.AddFlight(flight);
+            flight = FlightStorage.AddFlight(flight);*/
             return Created("", flight);
         }
 
@@ -49,7 +65,13 @@ namespace FlightPlanner.Controllers
         [HttpDelete]
         public IActionResult DeleteFlight(int id)
         {
-            FlightStorage.DeleteFlight(id);
+            var flight = _context.Flights.FirstOrDefault(f => f.Id == id);
+            if (flight != null)
+            {
+                _context.Flights.Remove(flight);
+                _context.SaveChanges();
+            }
+            //FlightStorage.DeleteFlight(id);
             return Ok(id);
         }
     }
---
+++ FlightPlanner/Migrations/20220930162746_Init.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FlightPlanner.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Airports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Country = table.Column<string>(type: "TEXT", nullable: true),
+                    City = table.Column<string>(type: "TEXT", nullable: true),
+                    AirportCode = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Airports", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Flights",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FromId = table.Column<int>(type: "INTEGER", nullable: true),
+                    ToId = table.Column<int>(type: "INTEGER", nullable: true),
+                    Carrier = table.Column<string>(type: "TEXT", nullable: true),
+                    DepartureTime = table.Column<string>(type: "TEXT", nullable: true),
+                    ArrivalTime = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flights", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Flights_Airports_FromId",
+                        column: x => x.FromId,
+                        principalTable: "Airports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Flights_Airports_ToId",
+                        column: x => x.ToId,
+                        principalTable: "Airports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_FromId",
+                table: "Flights",
+                column: "FromId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_ToId",
+                table: "Flights",
+                column: "ToId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Flights");
+
+            migrationBuilder.DropTable(
+                name: "Airports");
+        }
+    }
+}
